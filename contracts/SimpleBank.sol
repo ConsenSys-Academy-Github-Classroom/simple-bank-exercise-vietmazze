3140108eaa9fc14453b5011e01231554130f3618
vietmazze <dqnguyen2011@gmail.com>
2021-09-30 17:48:04 -0400

    update

--- contracts/SimpleBank.sol
+++ contracts/SimpleBank.sol
@@ -14,7 +14,7 @@ contract SimpleBank {
     
     // Fill in the visibility keyword. 
     // Hint: We want to protect our users balance from other contracts
-    mapping (address => uint) private balances ;
+    mapping (address => uint) internal balances ;
     
     // Fill in the visibility keyword
     // Hint: We want to create a getter function and allow contracts to be able
@@ -38,6 +38,16 @@ contract SimpleBank {
     // Hint: it should take 3 arguments: an accountAddress, withdrawAmount and a newBalance 
     event LogWithdrawal(address accountAddress, uint withdrawAmount, uint newBalance);
 
+
+    modifier onlyOwner(){
+      require(enrolled[msg.sender],"not enroll");
+      _;
+    }
+
+    modifier enoughBalance(uint withdrawAmount) {
+      require(balances[msg.sender]>= withdrawAmount, "not enough balance");
+      _;
+    }
     /* Functions
      */
 
@@ -52,10 +62,11 @@ contract SimpleBank {
 
     /// @notice Get balance
     /// @return The balance of the user
-    function getBalance() public returns (uint) {
+    function getBalance() public view returns (uint) {
       // 1. A SPECIAL KEYWORD prevents function from editing state variables;
       //    allows function to run locally/off blockchain
       // 2. Get the balance of the sender of this transaction
+      return balances[msg.sender];
     }
 
     /// @notice Enroll a customer with the bank
@@ -63,38 +74,46 @@ contract SimpleBank {
     // Emit the appropriate event
     function enroll() public returns (bool){
       // 1. enroll of the sender of this transaction
+      enrolled[msg.sender]=true;
+      emit LogEnrolled(msg.sender);
     }
+
+   
 
     /// @notice Deposit ether into bank
     /// @return The balance of the user after the deposit is made
-    function deposit() public returns (uint) {
+    function deposit() public payable onlyOwner returns (uint) {
       // 1. Add the appropriate keyword so that this function can receive ether
     
       // 2. Users should be enrolled before they can make deposits
-
+      
       // 3. Add the amount to the user's balance. Hint: the amount can be
       //    accessed from of the global variable `msg`
-
+      balances[msg.sender] += msg.value; 
       // 4. Emit the appropriate event associated with this function
-
+      emit LogDepositMade(msg.sender,msg.value);
       // 5. return the balance of sndr of this transaction
+      return balances[msg.sender];
     }
 
     /// @notice Withdraw ether from bank
     /// @dev This does not return any excess ether sent to it
     /// @param withdrawAmount amount you want to withdraw
     /// @return The balance remaining for the user
-    function withdraw(uint withdrawAmount) public returns (uint) {
+    function withdraw(uint withdrawAmount) public onlyOwner enoughBalance(withdrawAmount) returns (uint) {
       // If the sender's balance is at least the amount they want to withdraw,
       // Subtract the amount from the sender's balance, and try to send that amount of ether
       // to the user attempting to withdraw. 
       // return the user's balance.
-
+      
       // 1. Use a require expression to guard/ensure sender has enough funds
 
       // 2. Transfer Eth to the sender and decrement the withdrawal amount from
       //    sender's balance
-
+      balances[msg.sender] -= withdrawAmount;
+      (bool success, bytes memory data) = msg.sender.call{value:withdrawAmount}("");
+      require(success,"failed to send eth");
       // 3. Emit the appropriate event for this message
+      emit LogWithdrawal(msg.sender,withdrawAmount,balances[msg.sender])
     }
 }
